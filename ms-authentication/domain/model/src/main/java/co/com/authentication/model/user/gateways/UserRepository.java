988819100c81ea1190c5c42da06d8b27e66eec54
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-24 18:35:20 -0500

    Convierto ms-authentication en carpeta normal del repositorio

---
+++ ms-authentication/domain/model/src/main/java/co/com/authentication/model/user/gateways/UserRepository.java
@@ -0,0 +1,4 @@
+package co.com.authentication.model.user.gateways;
+
+public interface UserRepository {
+}
---
+++ ms-authentication/domain/usecase/src/main/java/co/com/authentication/usecase/registeruser/RegisterUserUseCase.java
@@ -0,0 +1,6 @@
+package co.com.authentication.usecase.registeruser;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class RegisterUserUseCase {
+}
