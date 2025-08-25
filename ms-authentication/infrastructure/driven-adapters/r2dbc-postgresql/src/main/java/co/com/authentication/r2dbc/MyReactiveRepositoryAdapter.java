4c7a03ec70af132aa5e9b436dae236bd7fcf9a89
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-25 17:51:36 -0500

    feat(MyReactiveRepositoryAdapter):  se implementan los métodos en la clase

--- ms-authentication/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/authentication/r2dbc/MyReactiveRepositoryAdapter.java
+++ ms-authentication/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/authentication/r2dbc/MyReactiveRepositoryAdapter.java
@@ -1,23 +1,64 @@
 package co.com.authentication.r2dbc;
 
+import co.com.authentication.model.user.User;
+import co.com.authentication.model.user.gateways.UserRepository;
+import co.com.authentication.r2dbc.entity.UserEntity;
 import co.com.authentication.r2dbc.helper.ReactiveAdapterOperations;
+import reactor.core.publisher.Mono;
+
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MyReactiveRepositoryAdapter extends ReactiveAdapterOperations<
-    Object/* change for domain model */,
-    Object/* change for adapter model */,
-    String,
+    User/* change for domain model */,
+    UserEntity/* change for adapter model */,
+    Long,
     MyReactiveRepository
-> {
+> implements UserRepository {
     public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper) {
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
-        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+        super(repository, mapper, d -> mapper.map(d, User.class/* change for domain model */));
+    }
+
+    @Override
+    public Mono<User> findByUserId(String userId) {
+        return repository.findByUserId(userId)
+                .map(entity -> mapper.map(entity, User.class));
+    }
+
+   
+    @Override
+    public Mono<User> findByEmail(String email) {
+        return repository.findByEmail(email)
+                .map(entity -> mapper.map(entity, User.class));
+    }
+
+    @Override
+    public Mono<User> findByDocumentNumber(String documentNumber) {
+        return repository.findByDocumentNumber(documentNumber)
+                .map(entity -> mapper.map(entity, User.class));
+    }
+
+    @Override
+    public Mono<Boolean> existsByEmail(String email) {
+         return repository.findByEmail(email)
+                     .hasElement();
+    }
+
+    @Override
+    public Mono<Boolean> existsByDocumentNumber(String documentNumber) {
+        return repository.findByDocumentNumber(documentNumber)
+                     .hasElement();
+    }
+
+    @Override
+    public Mono<Void> deleteById(Long id) {
+        return repository.deleteById(id);
     }
 
 }
