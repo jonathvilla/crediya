533e09582792f996334a2ab800d43da476fa1a8e
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-25 17:43:30 -0500

    feat(User):  se crea clase en el model

--- ms-authentication/domain/model/src/main/java/co/com/authentication/model/user/User.java
+++ ms-authentication/domain/model/src/main/java/co/com/authentication/model/user/User.java
@@ -1,14 +1,43 @@
 package co.com.authentication.model.user;
 import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-//@NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class User {
+    private String userId;
+    private String names;
+    private String lastName;
+    private LocalDate birthDate;
+    private String documentType;
+    private String documentNumber;
+   
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private Double baseSalary;
+    private String occupation;
+    private String company;
+    private LocalDateTime creationDate;
+    private LocalDateTime updateDate;
+
+   
+    public boolean isValidAge() {
+        return birthDate.isBefore((LocalDate.now().minusYears(18)));
+    }
+
+    public String getFullName() {
+        return names + " " + lastName;
+    }
+
 }
