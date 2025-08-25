98e4dfbdd807344a8f1d3f5b754b5f5d69c4673c
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-25 17:47:20 -0500

    feat(MyReactiveRepository):  se crean las consultas a la base de datos

--- ms-authentication/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/authentication/r2dbc/MyReactiveRepository.java
+++ ms-authentication/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/authentication/r2dbc/MyReactiveRepository.java
@@ -1,9 +1,21 @@
 package co.com.authentication.r2dbc;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import co.com.authentication.r2dbc.entity.UserEntity;
+import reactor.core.publisher.Mono;
+
 // TODO: This file is just an example, you should delete or modify it
-public interface MyReactiveRepository extends ReactiveCrudRepository<Object, String>, ReactiveQueryByExampleExecutor<Object> {
+public interface MyReactiveRepository extends ReactiveCrudRepository<UserEntity, Long>, ReactiveQueryByExampleExecutor<UserEntity> {
+     @Query("SELECT * FROM users WHERE user_id = :userId")
+    Mono<UserEntity> findByUserId(String userId);
+
+    @Query("SELECT * FROM users WHERE correo_electronico = :email")
+    Mono<UserEntity> findByEmail(String email);
+
+    @Query("SELECT * FROM users WHERE numero_documento = :documentNumber")
+    Mono<UserEntity> findByDocumentNumber(String documentNumber);
 
 }
