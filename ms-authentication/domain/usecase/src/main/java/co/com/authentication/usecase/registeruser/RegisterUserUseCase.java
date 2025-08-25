40b3d896bfe5126fc941aba84cac2e742b8dfe1c
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-25 17:46:19 -0500

    feat(RegisterUserUseCase):  se implementan validaciones en el caso de uso

--- ms-authentication/domain/usecase/src/main/java/co/com/authentication/usecase/registeruser/RegisterUserUseCase.java
+++ ms-authentication/domain/usecase/src/main/java/co/com/authentication/usecase/registeruser/RegisterUserUseCase.java
@@ -1,6 +1,65 @@
 package co.com.authentication.usecase.registeruser;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import co.com.authentication.model.user.User;
+import co.com.authentication.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class RegisterUserUseCase {
+
+    private final UserRepository userRepository;
+    
+    public Mono<User> execute(User user) {
+        return validateUserData(user)
+                .then(checkUserDoesNotExist(user))
+                .then(createUser(user));
+    }
+    
+    private Mono<Void> validateUserData(User user) {
+        if (!user.isValidAge()) {
+            return Mono.error(new RuntimeException("El usuario debe ser mayor de edad"));
+        }
+        
+        if (user.getBaseSalary() == null || user.getBaseSalary() < 0) {
+            return Mono.error(new RuntimeException("El salario base debe ser mayor a 0"));
+        }
+        
+        if (user.getBaseSalary() > 15000000) {
+            return Mono.error(new RuntimeException("El salario base no puede exceder 15,000,000"));
+        }
+        
+        return Mono.empty();
+    }
+    
+    private Mono<Void> checkUserDoesNotExist(User user) {
+        return userRepository.existsByEmail(user.getEmail())
+                .flatMap(emailExists -> {
+                    if (emailExists) {
+                        return Mono.error(new RuntimeException("Correo electrónico ya registrado"));
+                    }
+
+                    return userRepository.existsByDocumentNumber(user.getDocumentNumber())
+                            .flatMap(docExists -> {
+                                if (docExists) {
+                                    return Mono
+                                            .error(new RuntimeException("Número de documento ya registrado"));
+                                }
+                                return Mono.empty();
+                            });
+                });
+    }
+    
+    private Mono<User> createUser(User user) {
+        User newUser = user.toBuilder()
+                .userId(UUID.randomUUID().toString())
+                .creationDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+        
+        return userRepository.save(newUser);
+    }
+    
 }
