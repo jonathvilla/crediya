6175c9aee60fb9008f0786ac25460fec605f8944
jonath.mercado <jonath.mercado@iesonline.com.co>
2025-08-25 17:44:46 -0500

    feat(UserRepository):  se definen los métodos  de la clase

--- ms-authentication/domain/model/src/main/java/co/com/authentication/model/user/gateways/UserRepository.java
+++ ms-authentication/domain/model/src/main/java/co/com/authentication/model/user/gateways/UserRepository.java
@@ -1,4 +1,22 @@
 package co.com.authentication.model.user.gateways;
 
+import co.com.authentication.model.user.User;
+import reactor.core.publisher.Mono;
+
 public interface UserRepository {
+    Mono<User> save(User user);
+
+    Mono<User> findById(Long id);
+
+    Mono<User> findByUserId(String userId);
+
+    Mono<User> findByEmail(String email);
+
+    Mono<User> findByDocumentNumber(String documentNumber);
+
+    Mono<Boolean> existsByEmail(String email);
+
+    Mono<Boolean> existsByDocumentNumber(String documentNumber);
+
+    Mono<Void> deleteById(Long id);
 }
